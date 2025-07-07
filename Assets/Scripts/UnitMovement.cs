0a9bac32ef70e453b5beb22ee5e51cb6ffed8bef
PavelOnishenko <120336683+PavelOnishenko@users.noreply.github.com>
2025-07-07 23:38:07 +0200

    Refactoring.

--- Assets/Scripts/UnitMovement.cs
+++ Assets/Scripts/UnitMovement.cs
@@ -20,38 +20,20 @@ public class UnitMovement : MonoBehaviour
     {
         if (!target.HasValue) return;
 
-        Vector3 dir = (target.Value - transform.position).normalized;
-        Debug.Log($"dir = [{dir}].");
-        Vector3 nextStep = transform.position + dir * moveSpeed * Time.deltaTime;
-        Debug.Log($"nextStep = [{nextStep}].");
-
-        // Raycast forward to detect obstacles
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, obstacleRayLength, obstacleMask);
+        var dir = (target.Value - transform.position).normalized;
+        var nextStep = transform.position + dir * moveSpeed * Time.deltaTime;
+        var hit = Physics2D.Raycast(transform.position, dir, obstacleRayLength, obstacleMask);
         if (hit.collider != null || cooldown > 0)
         {
-            Debug.Log($"IN IF");
             if (hit.collider != null)
-            {
                 cooldown = 200;
-                Debug.Log($"cooldown = [{cooldown}].");
-            }
-            // Obstacle ahead — steer sideways
-            Vector3 right = Vector3.Cross(dir, Vector3.forward); // perpendicular
-            Debug.Log($"right = [{right}].");
+            var right = Vector3.Cross(dir, Vector3.forward); // perpendicular
             nextStep = transform.position + (dir + right * avoidanceStrength).normalized * moveSpeed * Time.deltaTime;
-            Debug.Log($"IN IF nextStep = [{nextStep}].");
             if(cooldown > 0) 
                 cooldown--;
-            Debug.Log($"cooldown decreased = [{cooldown}].");
         }
-        Debug.DrawRay(transform.position, dir * obstacleRayLength, hit.collider == null && cooldown == 0 ? Color.blue : Color.red);
-
         transform.position = nextStep;
-
         if (Vector3.Distance(transform.position, target.Value) < 0.1f)
-        {
             target = null;
-            Debug.Log($"Target is reached.");
-        }
     }
 }
